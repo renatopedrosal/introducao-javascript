f26990b5ad1158ead7afa875b5dc920a8d7556eb
Renato Pedrosa Leite <renatopedrosaleite.rp@gmail.com>
2021-12-30 17:20:03 -0300

    .

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,35 @@
-var titulo = document.querySelector(".titulo");
+var pacientes = document.querySelectorAll(".paciente");
 
-titulo.textContent = "Aparecida nutricionista";
+for (i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var tdPeso = paciente.querySelector(".info-peso");
+      var tdAltura = paciente.querySelector(".info-altura");
 
-var paciente = document.querySelector("#primeiro-paciente");
+      var tdImc = paciente.querySelector(".info-imc");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+      var peso = tdPeso.textContent;
+      var altura = tdAltura.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+      var validaPeso = true;
+      var validaAltura = true;
 
-var imc = peso / (altura * altura);
+      if (peso <= 0 || peso >= 300) {
+            console.log("Peso Inválido")
+            tdImc.textContent = "Peso Inválido";
+            validaPeso = false;
+            paciente.classList.add("paciente-invalido");
+      }
 
-var tdImc = document.querySelector(".info-imc");
-tdImc.textContent = imc.toFixed(2); 
+      if (altura <= 1 || altura >= 3) {
+            console.log("Altura Inválida")
+            tdImc.textContent = "Altura Inválida";
+            validaAltura = false;
+            paciente.classList.add("paciente-invalido");
+      }
 
-var validaPeso = true;
-var validaAltura = true;
-
-if (peso <= 0 || peso >= 300) {
-      console.log("Peso Inválido")
-      tdImc.textContent = "Peso Inválido";
-      validaPeso = false;
-}
-
-if (altura <= 1 || altura >= 3) {
-      console.log("Altura Inválido")
-      tdImc.textContent = "Altura Inválida";
-      validaAltura = false;
+      if (validaPeso && validaAltura){
+            var imc = peso / (altura * altura);
+            tdImc.textContent = imc.toFixed(2);
+            console.log(imc); 
+      }
 }
